8836a8162cefe47fcfd69e531630273ee427697d
thieugia882014 <70931357+thieugia882014@users.noreply.github.com>
2022-08-19 09:55:13 +0700

    Add files via upload

---
+++ demo2/src/main/java/com/example/demo2/utils/ConnectionUtils.java
@@ -0,0 +1,37 @@
+package com.example.demo2.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtils {
+
+    private static Connection connection;
+
+    public synchronized static Connection getConnection() {
+        String url = "jdbc:mysql://localhost:3306/demo2";
+        String user = "root";
+        String pass = "";
+        if (connection == null) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                connection = DriverManager.getConnection(url, user, pass);
+            } catch (SQLException | ClassNotFoundException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+        return connection;
+    }
+
+    public void closeConnection() {
+        try {
+            connection.close();
+            connection = null;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            connection = null;
+        }
+    }
+}
---
+++ demo2/target/demo2-1.0-SNAPSHOT/login.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+<form action="login" method="post">
+    Username: <input type="text" name="username">
+    Password: <input type="text" name="password">
+    <input type="submit" value="Login">
+</form>
+</body>
+</html>
